e7f6ba3c0c9162879e4b4a2e4bb40a0a4e5e1e93
Maurici Abad Gutiérrez <hello@mauriciabad.com>
2019-04-20 18:59:43 +0200

    Update hackNights.js

--- hackNights.js
+++ hackNights.js
@@ -16,32 +16,49 @@ var hackNights = {
       date: "2019-02-15 20:00",
       name: "Episode I",
       theme: { emoji: "💻", name: "Intro" },
+      schedule: [
+        { hour: "19:45", name: "Doors opening" },
+        { hour: "20:30", name: "Opening speach" },
+        { hour: "23:00", name: "Midnight Snack" },
+        { hour: "04:00", name: "Project Demos (Free)" },
+        { hour: "06:00", name: "Closing" },
+      ],
+      summary: "",
     },
     {
       date: "2019-03-01 20:00",
       name: "Episode II",
       theme: { emoji: "🎉", name: "Carnaval" },
+      schedule: [
+        { hour: "19:45", name: "Doors opening" },
+        { hour: "20:30", name: "Opening speach" },
+        { hour: "23:00", name: "Midnight Snack" },
+        { hour: "00:00", name: "Bottle Flip Challenge" },
+        { hour: "06:00", name: "Closing" },
+      ],
+      summary: "",
     },
     {
       date: "2019-03-29 20:00",
       name: "Episode III",
       theme: { emoji: "🎶", name: "Music" },
-      applyUrl: "https://hackersatupc.typeform.com/to/qpb6R2",
       schedule: [
-        { hour: "19:45", name:"Doors opening" },
-        { hour: "20:30", name:"Opening speach" },
-        { hour: "22:00", name:"Workshop: Intro Node" },
-        { hour: "23:00", name:"Midnight Snack" },
-        { hour: "00:00", name:"Bottle Flip Challenge" },
-        { hour: "04:00", name:"Project Demos (Free)" },
-        { hour: "06:00", name:"Closing" },
+        { hour: "19:45", name: "Doors opening" },
+        { hour: "00:00", name: "Workshop: Intro Node" },
+        { hour: "06:00", name: "Closing" },
       ],
-      summary: "As we announced a few days ago, this time we won't be able to provide dinner, but you will be allowed to eat in the room. However, we will provide snacks and beverage to hack all night long! If possible, we ask you to bring your own plastic cup in order to refill your drink (long live to recycling ♻😊). If you came to any previous edition, remember to wear your HackNights t-shirt!"
+      summary: "This time we won't be able to provide dinner, but you will be allowed to eat in the room. However, we will provide snacks and beverage to hack all night long! If possible, we ask you to bring your own plastic cup in order to refill your drink (long live to recycling ♻😊). If you came to any previous edition, remember to wear your HackNights t-shirt!"
     },
     {
       date: "2019-04-12 20:00",
       name: "Episode IV",
-      theme: { emoji: "🌴", name: "???" },
+      theme: { emoji: "😁", name: "Free" },
+      schedule: [
+        { hour: "19:45", name: "Doors opening" },
+        { hour: "22:00", name: "Workshop: Intro to APIs" },
+        { hour: "06:00", name: "Closing" },
+      ],
+      summary: "This time we won't be able to provide dinner, but you will be allowed to eat in the room. However, we will provide snacks and beverage to hack all night long! If possible, we ask you to bring your own plastic cup in order to refill your drink (long live to recycling ♻😊). If you came to any previous edition, remember to wear your HackNights t-shirt!"
     },
     {
       date: "2019-05-24 20:00",
@@ -80,5 +97,5 @@ for (const event of hackNights.events) {
   let date = new Date(event.date);
   event.done = date < now;
 
-  event.next = event == hackNights.nextEvent; 
+  event.next = event == hackNights.nextEvent;
 }
