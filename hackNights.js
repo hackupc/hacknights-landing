fda1c58e46477a7b5a5cbdba0833192d154b6a7e
AwaKT <gerardm27@gmail.com>
2023-08-30 18:53:19 +0200

    added ep3

--- hackNights.js
+++ hackNights.js
@@ -39,22 +39,22 @@ var hackNights = {
       ],
       summary: "",
     },
-    
+    {
+      date: "2023-09-29 21:00",
+      name: "Episodi III",
+      theme: { emoji: "❓", name: "TBD" },
+      applyUrl: "https://hackersatupc.typeform.com/ht-2023-ep-3",
+      schedule: [
+        { hour: "21:00", name: "Registre" },
+        { hour: "21:15", name: "Cerimònia d'obertura" },
+        { hour: "21:30", name: "Workshop" },
+        { hour: "00:00", name: "Midnight Snack" },
+        { hour: "05:00", name: "Cerimònia de clausura" },
+      ],
+      summary: "",
+    },
   ],
-  nextEvent: {
-    date: "2023-09-29 21:00",
-    name: "Episodi III",
-    theme: { emoji: "❓", name: "TBD" },
-    applyUrl: "https://hackersatupc.typeform.com/ht-2023-ep-3",
-    schedule: [
-      { hour: "21:00", name: "Registre" },
-      { hour: "21:15", name: "Cerimònia d'obertura" },
-      { hour: "21:30", name: "Workshop" },
-      { hour: "00:00", name: "Midnight Snack" },
-      { hour: "05:00", name: "Cerimònia de clausura" },
-    ],
-    summary: "",
-  },
+  nextEvent: undefined,
 };
 
 // sort events
