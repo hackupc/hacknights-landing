9a7a9c3a1b17b6fb4c08ee9412ea6b49dce05cd8
Yaoyao Guo <168144675+yaoguz@users.noreply.github.com>
2026-09-25 07:44:00 +0200

    2026 ep2 + eliminar duplicacio textos (#14)

--- hackNights.js
+++ hackNights.js
@@ -1,6 +1,6 @@
 // This file declares a variable with all HackNights info
 var hackNights = {
-  season: "2024",
+  season: "2026",
   defaultEvent: {
     date: "undefined",
     name: "Episode Extra",
@@ -16,12 +16,27 @@ var hackNights = {
       date: "2026-02-27 21:00",
       name: "Episodi I (2026)",
       theme: { emoji: "🤖", name: "Estudiar en temps de IA" },
-      applyUrl: "https://hackersatupc.typeform.com/hacknights2026",
+      applyUrl: "",
       schedule: [
         { hour: "21:00", name: "Registre" },
         { hour: "21:15", name: "Cerimònia d'obertura" },
-        { hour: "21:30", name: "Workshop: AI tools for university and school." },
+        { hour: "21:30", name: "Workshop: AI tools for university and school" },
         { hour: "00:00", name: "Midnight Snack" },
+        { hour: "05:00", name: "Cerimònia de clausura" },
+      ],
+      summary: ""
+    },
+    {
+      date: "2026-10-02 21:00",
+      name: "Episodi II (2026)",
+      theme: { emoji: "🤖", name: "Hackers@UPC X JetBrains" },
+      applyUrl: "https://hackersatupc.typeform.com/to/ToRHdqnJ?fbclid=PAcGRvZgRleHRuA2FlbQIxMQBzcnRjBmFwcF9pZAwyNTYyODEwNDA1NTgAAadpMiAXVP7pX698P0PzNfSnIQnHWwGsvOQRVrnjgSaIht2kIxr1uQ7rXcMtfA_aem_Rc10ouaN2I76CBRud8aI6g&typeform-source=l.instagram.com",
+      schedule: [
+        { hour: "21:00", name: "Registre" },
+        { hour: "21:50", name: "Cerimònia d'obertura" },
+        { hour: "22:00", name: "Workshop: Tap In: Building Real-World Android Apps with NFC" },
+        { hour: "23:00", name: "Sopar: pizzes" },
+        { hour: "01:00", name: "Midnight Snack" },
         { hour: "05:00", name: "Cerimònia de clausura" },
       ],
       summary: ""
